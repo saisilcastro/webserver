f38dc3ea491b24bbec2639da514c382f88cbdc3f
MisterCoder <saisilcastro@gmail.com>
2024-08-06 12:19:36 -0300

    error handling

--- src/Server.cpp
+++ src/Server.cpp
@@ -183,8 +183,9 @@ void  Server::contentMaker(int client, string protocol, string connection, void 
 }
 
 void Server::response(int client, string path, string protocol) {
-    size_t pos = path.rfind(".");
-    Stream stream("");
+    size_t  pos = path.rfind(".");
+    Stream  stream("");
+    string  status = " 200 OK";
 
     if (master.isMethod() != DELETE) {
         mimeMaker(path);
@@ -192,6 +193,7 @@ void Server::response(int client, string path, string protocol) {
             string index = findDirectiveValue("index");
             if (MaxBodySize < master.getFileLen() && master.getFileLen() > 0) {
                 stream.loadFile(root + '/' + "413.html");
+                status = " 413 Content Too Large";
             }
             else if (!index.empty())
                 stream.loadFile(root + '/' + index);
@@ -201,12 +203,15 @@ void Server::response(int client, string path, string protocol) {
             if (master.isMethod() == POST && MaxBodySize > master.getFileLen()) {
                 contentMaker(client, protocol + " 200 OK", "keep-alive", stream.getStream(), stream.streamSize());
                 path = "/413.html";
+                status = " 413 Content Too Large";
             }
             else if ((pos = path.find(".")) != string::npos) {
                 if (path.find("?") != string::npos) {
                     if ((path.find(".php") != string::npos && path[pos + 4] != '?') ||
-                        (path.find(".py") != string::npos && path[pos + 3]))
+                        (path.find(".py") != string::npos && path[pos + 3])) {
                         path = "/404.html";
+                        status = " 404 Not Found";
+                    }
                     else {
                         if (path.find(".php") != string::npos)
                             path = path.substr(0, pos + 4);
@@ -215,8 +220,10 @@ void Server::response(int client, string path, string protocol) {
                     }
                 } else {
                     if ((path.find(".php") != string::npos && path.length() > pos + 4) ||
-                        (path.find(".py") != string::npos && path.length() > pos + 3))
+                        (path.find(".py") != string::npos && path.length() > pos + 3)) {
                         path = "/404.html";
+                        status = " 404 Not Found";
+                    }
                 }
             }
             stream.loadFile(root + path);
@@ -228,7 +235,7 @@ void Server::response(int client, string path, string protocol) {
         if (!stat(file.c_str(), &mStat) && mStat.st_size > 0)
             remove(file.c_str());
     }
-    contentMaker(client, protocol + " 200 OK", "keep-alive", stream.getStream(), stream.streamSize());
+    contentMaker(client, protocol + status, "keep-alive", stream.getStream(), stream.streamSize());
 }
 
 void    Server::requestTreat(int client, string data) {
